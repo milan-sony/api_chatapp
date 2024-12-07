aafe5ae1fd4568e8d0f87f4be01424e19499ca82
Milan Sony <milansonyofficial@gmail.com>
2024-12-07 10:52:21 +0530

    specified node and npm version (engine)

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.listen
-
 app.listen((process.env.PORT || 5000), () => {
     console.log(`\nServer listening on port: ${process.env.PORT || 5000}`)
 });
