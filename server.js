935580a8880ec70f12de1f09ea34da8494bbdfa4
Milan Sony <milansonyofficial@gmail.com>
2024-12-07 10:57:16 +0530

    nodemon added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get("/", (req, res)=>{
+    res.send("API - React Chat App")
+})
+
 app.listen((process.env.PORT || 5000), () => {
     console.log(`\nServer listening on port: ${process.env.PORT || 5000}`)
 });
