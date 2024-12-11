e9ea176819ad83d6586705ec47c59fd0ef3e5e9d
Milan Sony <milansonyofficial@gmail.com>
2024-12-11 16:39:51 +0530

    body parser removed and added express.json()

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ const db = require('./config/db');
 db.connect();
 
 // body parser
-const bodyParser = require('body-parser')
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Base URL (/)
 const routes = require("./routes")
