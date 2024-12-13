f80b5f51aba9812ea07299869d96f0c04344d433
Milan Sony <milansonyofficial@gmail.com>
2024-12-13 16:20:58 +0530

    api routes modified

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,14 +2,16 @@ const express = require("express")
 const router = express.Router()
 
 const userRoutes = require("./userRoutes")
+const messageRoutes = require("./messageRoutes")
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
     res.status(200).send({
         status: 200,
         message: "Chat API V1"
     })
 })
 
-router.use('/user', userRoutes)
+router.use("/user", userRoutes)
+router.use("/message", messageRoutes)
 
 module.exports = router
