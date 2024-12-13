5226e4e235da5928ab514aecec9ddc8f28c2bbab
Milan Sony <milansonyofficial@gmail.com>
2024-12-13 17:29:04 +0530

    feat: get all the messages between a users one-one

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -1,16 +1,40 @@
+const messagemodel = require("../model/messagemodel")
 const usermodel = require("../model/userModel")
 
 module.exports = {
     getUsersForSidebar: async (req, res) => {
         try {
             const loggedUserId = req.user._id // user id is getting from the verifyToken
-            const filteredUsers = await usermodel.find({_id: {$ne: loggedUserId}}).select("-password -__v") //getting all the user data (excluding password),except the current user (`$ne` - not equal to)
+            const filteredUsers = await usermodel.find({ _id: { $ne: loggedUserId } }).select("-password -__v") //getting all the user data (excluding password),except the current user (`$ne` - not equal to)
 
             console.log("Filtered users: ", filteredUsers)
 
             res.status(200).send({
                 status: 200,
                 message: filteredUsers
+            })
+        } catch (error) {
+            res.status(500).send({
+                status: 500,
+                message: "Internal server error"
+            })
+        }
+    },
+
+    getMessages: async (req, res) => {
+        try {
+            const { id: userToChatId } = req.parms //gets the id from the parms and saves it to userToChatId
+            const senderId = req.user._id  //current user id
+
+            const messages = await messagemodel.find({
+                $or:[
+                    {senderId:senderId, receiverId:userToChatId},
+                    {senderId:userToChatId, receiverId:senderId}
+                ]
+            }) //find all the messages between the sender and the receiver or vice vesa
+            res.status(200).send({
+                status: 200,
+                message: messages
             })
         } catch (error) {
             res.status(500).send({
--- routes/api/messageRoutes.js
+++ routes/api/messageRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const verifyToken = require('../../middleware/verifyToken')
-const { getUsersForSidebar } = require('../../controllers/messageControllers')
+const { getUsersForSidebar, getMessages } = require('../../controllers/messageControllers')
 const router = express.Router()
 
-router.get("/users", verifyToken, getUsersForSidebar)
+router.get("/users", verifyToken, getUsersForSidebar) //get all the users (except the current user) to display on the sidebar
+router.get("/:id", verifyToken, getMessages) //get all the message with a particular user
 
 module.exports = router
