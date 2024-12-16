5987e41f015655f4cd2fa6ec28fee8eae5309064
Milan Sony <milansonyofficial@gmail.com>
2024-12-16 14:29:08 +0530

    fix: cors issue (blocked issue)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+
 // .env
 require("dotenv").config()
 
@@ -10,6 +11,13 @@ db.connect();
 
 // body parser
 app.use(express.json())
+
+// cors
+const cors = require("cors")
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 // cookie parser
 const cookieParser = require("cookie-parser")
