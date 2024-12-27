e98f4d5f6a41cd9d6e5069586832c49d18a4789a
Milan Sony <milansonyofficial@gmail.com>
2024-12-27 16:58:24 +0530

    console.log removed

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -7,9 +7,6 @@ module.exports = {
         try {
             const loggedUserId = req.user._id // user id is getting from the verifyToken
             const filteredUsers = await usermodel.find({ _id: { $ne: loggedUserId } }).select("-password -__v") //getting all the user data (excluding password),except the current user (`$ne` - not equal to)
-
-            console.log("Filtered users: ", filteredUsers)
-
             res.status(200).send({
                 status: 200,
                 message: filteredUsers
